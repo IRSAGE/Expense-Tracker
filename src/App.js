5917fa45fee757bfff269ff0403bb4aa92e090a8
IRSAGE <sibo.egide98@gmail.com>
2022-08-04 16:43:32 +0200

    Creating the Adding new expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expense from "./components/Expenses/Expense";
+import NewExpense from "./components/Expenses/NewExpence/NewExpense";
 
 function App() {
   const expenses = [
@@ -24,6 +25,7 @@ function App() {
   ];
   return (
     <div>
+      <NewExpense />
       <Expense expenses={expenses} />
     </div>
   );
