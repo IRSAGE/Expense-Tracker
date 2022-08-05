bef1f779932bc253037c41da24fb8c3b28521515
IRSAGE <sibo.egide98@gmail.com>
2022-08-05 11:32:59 +0200

    Rendering elements basing on a condition

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -6,22 +6,33 @@ import ExpenseFilter from "./ExpenseFilter";
 
 const Expense = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.expenses.map((expense) => (
-        <ExpenceItem
-          title={expense.title}
-          date={expense.date}
-          amount={expense.amount}
-        />
-      ))}
+      {filteredExpenses.length === 0 ? (
+        <p>No Expenses Found in {filteredYear}</p>
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenceItem
+            key={expense.id}
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        ))
+      )}
     </Card>
   );
 };
