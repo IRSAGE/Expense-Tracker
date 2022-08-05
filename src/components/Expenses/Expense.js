09b81f858e97233de20a651b05050909290da98b
IRSAGE <sibo.egide98@gmail.com>
2022-08-05 10:40:46 +0200

    rendering an array of expenses

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -15,21 +15,13 @@ const Expense = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenceItem
-        title={props.expenses[0].title}
-        date={props.expenses[0].date}
-        amount={props.expenses[0].amount}
-      />
-      <ExpenceItem
-        title={props.expenses[1].title}
-        date={props.expenses[1].date}
-        amount={props.expenses[1].amount}
-      />
-      <ExpenceItem
-        title={props.expenses[2].title}
-        date={props.expenses[2].date}
-        amount={props.expenses[2].amount}
-      />
+      {props.expenses.map((expense) => (
+        <ExpenceItem
+          title={expense.title}
+          date={expense.date}
+          amount={expense.amount}
+        />
+      ))}
     </Card>
   );
 };
